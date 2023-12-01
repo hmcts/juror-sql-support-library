7a8e8eb8bbe1dc9d51e631aa5424fb22de8ffa1f
Ben Edwards <ben.edwards@cgi.com>
2023-12-01 12:22:30 +0000

    Loaded Spring Template

---
+++ src/main/java/uk/gov/hmcts/juror/support/sql/Application.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.juror.support.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
+public class Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

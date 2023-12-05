ccddd3ffde222cb8ee092af7db69c569bc83f5ea
Matt Smith <matt.smith@cgi.com>
2023-12-05 16:37:12 +0000

    -Removed PoliceCheckRepository.java -Removed Querydsl from PoolCommentRepository

--- src/main/java/uk/gov/hmcts/juror/support/sql/repository/PoliceCheckRepository.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.hmcts.juror.support.sql.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PoliceCheckRepository {
-}
--- src/main/java/uk/gov/hmcts/juror/support/sql/repository/PoolCommentRepository.java
+++ src/main/java/uk/gov/hmcts/juror/support/sql/repository/PoolCommentRepository.java
@@ -1,12 +1,10 @@
 package uk.gov.hmcts.juror.support.sql.repository;
 
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.juror.support.sql.entity.PoolComment;
 
 @Repository
-public interface PoolCommentRepository extends CrudRepository<PoolComment, Long>,
-        QuerydslPredicateExecutor<PoolComment> {
+public interface PoolCommentRepository extends CrudRepository<PoolComment, Long> {
 
 }
